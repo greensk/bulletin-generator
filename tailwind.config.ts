b68f1892c897bdfa262bd3319892134733820513
Grigory Kosheev <grigory@kosheev.com>
2024-11-15 16:36:07 +0700

    init konsta

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from "tailwindcss";
+const konstaConfig = require('konsta/config');
 
-export default {
+export default konstaConfig({
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'media',
   theme: {
     extend: {
       colors: {
@@ -15,4 +17,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+}) satisfies Config;
