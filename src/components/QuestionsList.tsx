09059a93885f9aa33b8d83ac64637deb0c05d376
Grigory Kosheev <grigory@kosheev.com>
2024-11-18 17:24:56 +0700

    improve add question button

--- src/components/QuestionsList.tsx
+++ src/components/QuestionsList.tsx
@@ -4,9 +4,12 @@ import { useState } from 'react'
 import {
   Block,
   Button,
+  Fab,
   Navbar,
   Page
 } from 'konsta/react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 import QueestionsItem from './QuestionsItem'
 
@@ -18,26 +21,33 @@ export default function QuestionsList() {
         title="Повестка дня"
       />
 
-      {
-        theList.map((listItem, listItemIndex) => {
-          return <div key={ listItemIndex }>
-            {
-              QueestionsItem({
-                number: listItemIndex + 1,
-                content: listItem,
-                onSetContent: (content: string) => {
-                  setTheList(theList.splice(listItemIndex, 1, content))
-                }
-              })
-            }
-          </div>
-        })
-      }
-      <Block strong outlineIos className="space-y-2">
-        <Button onClick={() => {
-          setTheList([...theList, ''])
-        }}>Добавить вопрос</Button>
-      </Block>
+      <div className="relative pb-12">
+        {
+          theList.map((listItem, listItemIndex) => {
+            return <div key={ listItemIndex }>
+              {
+                QueestionsItem({
+                  number: listItemIndex + 1,
+                  content: listItem,
+                  onSetContent: (content: string) => {
+                    setTheList(theList.map((i, index) => index === listItemIndex ? content: i))
+                  }
+                })
+              }
+            </div>
+          })
+        }
+        <Fab
+          className="absolute left-1/2 bottom-4-safe transform -translate-x-1/2 z-20"
+          icon={<FontAwesomeIcon icon={faPlus} className="relative -top-1" />}
+          text="Добавить вопрос"
+          textPosition="after"
+          touchRipple={ true }
+          onClick={() => {
+            setTheList([...theList, ''])
+          }}
+        />
+      </div>
     </Page>
   )
 }
