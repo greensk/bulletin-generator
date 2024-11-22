dcb1290118316c34431647547361b5ac973b9156
Grigory Kosheev <grigory@kosheev.com>
2024-11-22 11:26:44 +0700

    add meeting type switcher

--- src/components/Bulletin.tsx
+++ src/components/Bulletin.tsx
@@ -3,11 +3,33 @@
 import QuestionsList from './QuestionsList'
 import GeneralForm from './GeneralForm'
 
+import { Meeting } from '../types'
+
 import { useState } from 'react'
 
 type Step = 'general' | 'questions'
 
 export default function Bulleting() {
+  const [meeting, setMeeting] = useState<Meeting>({
+    address: {
+      city: '',
+      street: '',
+      house: ''
+    },
+    initiators: [],
+    questions: {},
+    meetingType: 'combined',
+    public: {
+      place: '',
+      date: '',
+      time: ''
+    },
+    distant: {
+      startDatetime: '',
+      endDatetime: '',
+      reception: ''
+    }  
+  })
   const [currentStep, setCurrentStep] = useState<Step>('general')
   return (
     <div>
@@ -17,6 +39,8 @@ export default function Bulleting() {
             setCurrentStep('questions')
           }
         }
+        meeting={ meeting }
+        setMeeting={ setMeeting }
       /> : <></> }
       { currentStep === 'questions' ?  <QuestionsList
         onBack={
--- src/components/GeneralForm.tsx
+++ src/components/GeneralForm.tsx
@@ -6,13 +6,24 @@ import {
   Button,
   List,
   ListInput,
+  ListItem,
   Navbar,
-  Page
+  Page,
+  Radio
 } from 'konsta/react'
+import { Meeting } from '../types'
+import {
+  Dispatch,
+  SetStateAction
+} from 'react'
 
 type GeneralFormProps = {
   onNext: () => void
+  meeting: Meeting
+  setMeeting: Dispatch<SetStateAction<Meeting>>
 }
+
+type MeetingType = 'intramural' | 'distant' | 'combined'
 
 export default function GeneralForm(props: GeneralFormProps) {
   return <Page>
@@ -39,6 +50,47 @@ export default function GeneralForm(props: GeneralFormProps) {
         placeholder="20"
       />
     </List>
+
+    <BlockTitle>Способ проведения собрания</BlockTitle>
+    <List strongIos insetIos>
+      <ListItem
+        label
+        title="Очное"
+        media={
+          <Radio
+            component="div"
+            value="public"
+            checked={props.meeting.meetingType === 'public'}
+            onChange={e => props.setMeeting( Object.assign({}, props.meeting, { meetingType: 'public' }) )}
+          />
+        }
+      />
+      <ListItem
+        label
+        title="Заочное"
+        media={
+          <Radio
+            component="div"
+            value="distant"
+            checked={props.meeting.meetingType === 'distant'}
+            onChange={e => props.setMeeting( Object.assign({}, props.meeting, { meetingType: 'distant' }) )}
+          />
+        }
+      />
+      <ListItem
+        label
+        title="Очно-заочное"
+        media={
+          <Radio
+            component="div"
+            value="combined"
+            checked={props.meeting.meetingType === 'combined'}
+            onChange={e => props.setMeeting( Object.assign({}, props.meeting, { meetingType: 'combined' }) )}
+          />
+        }
+      />
+    </List>
+
     <Block strong outlineIos className="space-y-2">
       <Button
         onClick={ props.onNext }
